43e4bf56f5a8ce4dfe1066d7986b4d84a072c260
ChoiMyungRak <audfkr0808@naver.com>
2023-03-10 00:59:57 +0900

    데이터 불러오기

--- app/src/main/java/com/lakue/petdelorder/MainActivity.kt
+++ app/src/main/java/com/lakue/petdelorder/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.lakue.petdelorder
 
+import android.content.res.AssetManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.lakue.petdelorder.databinding.ActivityMainBinding
+import com.opencsv.CSVReader
+import com.opencsv.exceptions.CsvException
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +21,21 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loadData()
+    }
 
+    @Throws(IOException::class, CsvException::class)
+    private fun loadData() {
+        val assetManager: AssetManager = getAssets()
+        val inputStream: InputStream = assetManager.open("petdel_order.csv")
+        val csvReader = CSVReader(InputStreamReader(inputStream, "UTF-8"))
+        val allContent = csvReader.readAll() as List<Array<String>>
+        for (content in allContent) {
+            val sb = StringBuilder("")
+            Log.d(
+                "csv",
+                "이름 : " + content[0] + " 가격: " + content[1] + " 바코드: " + content[2] + " 링크: " + content[3]
+            )
+        }
     }
 }
